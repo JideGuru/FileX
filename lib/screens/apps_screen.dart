eacf4c5c2fdb663894ba0e7a574485eb0584b516
jideguru <guruliciousjide@gmail.com>
2021-02-16 13:54:42 +0100

    [refactor]: refactored app screen

--- lib/screens/apps_screen.dart
+++ lib/screens/apps_screen.dart
@@ -2,67 +2,47 @@ import 'package:device_apps/device_apps.dart';
 import 'package:filex/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
-class AppScreen extends StatefulWidget {
-  @override
-  _AppScreenState createState() => _AppScreenState();
-}
-
-class _AppScreenState extends State<AppScreen> {
+class AppScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Installed Apps",
-        ),
+        title: Text("Installed Apps"),
       ),
       body: FutureBuilder<List<Application>>(
-          future: DeviceApps.getInstalledApplications(
-            onlyAppsWithLaunchIntent: true,
-            includeSystemApps: true,
-            includeAppIcons: true,
-          ),
-          builder: (context, snapshot) {
-            return snapshot == null
-                ? CustomLoader()
-                : snapshot.hasData
-                    ? ListView.separated(
-                        padding: EdgeInsets.only(left: 10),
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          Application app = snapshot.data[index];
-                          return ListTile(
-                            leading: app is ApplicationWithIcon
-                                ? Image.memory(
-                                    app.icon,
-                                    height: 40,
-                                    width: 40,
-                                  )
-                                : null,
-                            title: Text(
-                              app.appName,
-                            ),
-                            subtitle: Text("${app.packageName}"),
-                            onTap: () => DeviceApps.openApp(app.packageName),
-                          );
-                        },
-                        separatorBuilder: (BuildContext context, int index) {
-                          return Stack(
-                            children: <Widget>[
-                              Align(
-                                alignment: Alignment.centerRight,
-                                child: Container(
-                                  height: 1,
-                                  color: Theme.of(context).dividerColor,
-                                  width: MediaQuery.of(context).size.width - 80,
-                                ),
-                              ),
-                            ],
-                          );
-                        },
-                      )
-                    : CustomLoader();
-          }),
+        future: DeviceApps.getInstalledApplications(
+          onlyAppsWithLaunchIntent: true,
+          includeSystemApps: true,
+          includeAppIcons: true,
+        ),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Application> data = snapshot.data;
+            // Sort the App List on Alphabetical Order
+            data..sort((app1, app2)=>app1.appName.toLowerCase()
+                .compareTo(app2.appName.toLowerCase()));
+            return ListView.separated(
+              padding: EdgeInsets.only(left: 10),
+              itemCount: data.length,
+              itemBuilder: (BuildContext context, int index) {
+                Application app = data[index];
+                return ListTile(
+                  leading: app is ApplicationWithIcon
+                      ? Image.memory(app.icon, height: 40, width: 40)
+                      : null,
+                  title: Text(app.appName),
+                  subtitle: Text("${app.packageName}"),
+                  onTap: () => DeviceApps.openApp(app.packageName),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return CustomDivider();
+              },
+            );
+          }
+          return CustomLoader();
+        },
+      ),
     );
   }
 }
