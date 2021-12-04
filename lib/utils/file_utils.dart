aecd7076a5be496160ec192c98615556e8542dc0
Max <maximtsyba@gmail.com>
2021-12-04 22:27:58 +0200

    Optimize sortList() function

--- lib/utils/file_utils.dart
+++ lib/utils/file_utils.dart
@@ -142,78 +142,41 @@ class FileUtils {
     }
   }
 
-  static List<FileSystemEntity> sortList(
-      List<FileSystemEntity> list, int sort) {
+   static List<FileSystemEntity> sortList(
+     List<FileSystemEntity> list, int sort) {
     switch (sort) {
+
+      /// Sort by name
       case 0:
-        if (list.toString().contains('Directory')) {
-          list
-            ..sort((f1, f2) => basename(f1.path)
-                .toLowerCase()
-                .compareTo(basename(f2.path).toLowerCase()));
-          return list
-            ..sort((f1, f2) => f1
-                .toString()
-                .split(':')[0]
-                .toLowerCase()
-                .compareTo(f2.toString().split(':')[0].toLowerCase()));
-        } else {
-          return list
-            ..sort((f1, f2) => basename(f1.path)
-                .toLowerCase()
-                .compareTo(basename(f2.path).toLowerCase()));
-        }
+        list.sort((f1, f2) => basename(f1.path).toLowerCase().compareTo(basename(f2.path).toLowerCase()));
+        break;
 
       case 1:
-        list.sort((f1, f2) => basename(f1.path)
-            .toLowerCase()
-            .compareTo(basename(f2.path).toLowerCase()));
-        if (list.toString().contains('Directory')) {
-          list
-            ..sort((f1, f2) => f1
-                .toString()
-                .split(':')[0]
-                .toLowerCase()
-                .compareTo(f2.toString().split(':')[0].toLowerCase()));
-        }
-        return list.reversed.toList();
+        list.sort((f1, f2) => basename(f2.path).toLowerCase().compareTo(basename(f1.path).toLowerCase()));
+        break;
 
+      /// Sort by date
       case 2:
-        return list
-          ..sort((f1, f2) => FileSystemEntity.isFileSync(f1.path) &&
-                  FileSystemEntity.isFileSync(f2.path)
-              ? File(f1.path)
-                  .lastModifiedSync()
-                  .compareTo(File(f2.path).lastModifiedSync())
-              : 1);
+        list.sort((FileSystemEntity f1, FileSystemEntity f2) => f1.statSync().modified.compareTo(f2.statSync().modified));
+        break;
 
       case 3:
-        list
-          ..sort((f1, f2) => FileSystemEntity.isFileSync(f1.path) &&
-                  FileSystemEntity.isFileSync(f2.path)
-              ? File(f1.path)
-                  .lastModifiedSync()
-                  .compareTo(File(f2.path).lastModifiedSync())
-              : 1);
-        return list.reversed.toList();
+        list.sort((FileSystemEntity f1, FileSystemEntity f2) => f2.statSync().modified.compareTo(f1.statSync().modified));
+        break;
 
+      /// sort by size
       case 4:
-        list
-          ..sort((f1, f2) => FileSystemEntity.isFileSync(f1.path) &&
-                  FileSystemEntity.isFileSync(f2.path)
-              ? File(f1.path).lengthSync().compareTo(File(f2.path).lengthSync())
-              : 0);
-        return list.reversed.toList();
+        list.sort((FileSystemEntity f1, FileSystemEntity f2) => f2.statSync().size.compareTo(f1.statSync().size));
+        break;
 
       case 5:
-        return list
-          ..sort((f1, f2) => FileSystemEntity.isFileSync(f1.path) &&
-                  FileSystemEntity.isFileSync(f2.path)
-              ? File(f1.path).lengthSync().compareTo(File(f2.path).lengthSync())
-              : 0);
+        list.sort((FileSystemEntity f1, FileSystemEntity f2) => f1.statSync().size.compareTo(f2.statSync().size));
+        break;
 
       default:
-        return list..sort();
+        list.sort();
     }
+
+    return list;
   }
 }
